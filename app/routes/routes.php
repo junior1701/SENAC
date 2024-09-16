5f58888a52d6866fa5fdb5153046d7848e95fc14
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2024-09-16 16:22:12 -0400

    Commit

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -1,5 +1,11 @@
 <?php
 
+use app\controllers\ControllerCliente;
 use app\controllers\ControllerHome;
+use Slim\Routing\RouteCollectorProxy;
 
 $app->get('/', ControllerHome::class . ':home');
+
+$app->group('/cliente', function (RouteCollectorProxy $group) {
+    $group->get('/cadastro', ControllerCliente::class . ':cadastro');
+});
