67a50403b66085171f7ea62322b551fa7551001c
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2024-09-16 15:26:00 -0400

    Commit

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -3,9 +3,3 @@
 use app\controllers\ControllerHome;
 
 $app->get('/', ControllerHome::class . ':home');
-
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
