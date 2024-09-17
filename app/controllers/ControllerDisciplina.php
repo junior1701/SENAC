1b81b58e6bda9a1c4a235158eb7ebbbb5750030d
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2024-09-17 15:47:10 -0400

    Commit

--- app/controllers/ControllerDisciplina.php
+++ app/controllers/ControllerDisciplina.php
@@ -2,13 +2,19 @@
 
 namespace app\controllers;
 
+use app\database\builder\SelectQuery;
+
 class ControllerDisciplina extends Base
 {
     public function lista($request, $response)
     {
         try {
+            $disciplinas = (array) SelectQuery::select()
+                ->from('disciplina')
+                ->fetchAll();
             $TemplateData = [
-                'titulo' => 'Lista de Disciplinas'
+                'titulo' => 'Lista de Disciplinas',
+                'disciplinas' => $disciplinas
             ];
             return $this->getTwig()
                 ->render($response, $this->setView('listadisciplina'), $TemplateData)
@@ -18,6 +24,20 @@ class ControllerDisciplina extends Base
             throw new \Exception("Restrição: " . $e->getMessage(), 1);
         }
     }
-    public function cadastro($request, $response) {}
-    public function alterar($request, $response, $args) {}
+    public function cadastro($request, $response)
+    {
+        $TemplateData = [
+            'acao' => 'c',
+            'titulo' => 'Lista de Disciplinas'
+        ];
+    }
+    public function alterar($request, $response, $args)
+    {
+        var_dump($args);
+        die;
+        $TemplateData = [
+            'acao' => 'e',
+            'titulo' => 'Lista de Disciplinas'
+        ];
+    }
 }
