3a12b2baa1b8bede1c4088d86180f744b7a878a6
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2024-09-17 15:18:20 -0400

    Commit

--- app/traits/Template.php
+++ app/traits/Template.php
@@ -8,8 +8,13 @@ trait Template
 {
     public function getTwig()
     {
-        $twig = Twig::create(DIR_VIEW);
-        return $twig;
+        try {
+            $twig = Twig::create(DIR_VIEW);
+            return $twig;
+        } catch (\Exception $e) {
+            #RETORNAMOS O ERRO.
+            throw new \Exception("Erro: " . $e->getMessage(), 1);
+        }
     }
     public function setView($name)
     {
