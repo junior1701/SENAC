77db4769d797adc4519289b83cdfe85971a1c601
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2024-09-17 14:02:58 -0400

    cc

--- app/controllers/ControllerDisciplina.php
+++ app/controllers/ControllerDisciplina.php
@@ -4,7 +4,13 @@ namespace app\controllers;
 
 class ControllerDisciplina extends Base
 {
-    public function lista() {}
-    public function cadastro() {}
-    public function alterar() {}
+    public function lista($request, $response)
+    {
+        try {
+        } catch (\Exception $e) {
+            throw new \Exception("Restrição: " . $e->getMessage(), 1);
+        }
+    }
+    public function cadastro($request, $response) {}
+    public function alterar($request, $response, $args) {}
 }
